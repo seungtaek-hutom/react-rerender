7b1054e424f2a8d477954d361dd8b20df27468d8
seungtaek-hutom <seungtaek@hutom.io>
2021-08-12 18:00:15 +0900

    list-memo

--- src/components/list.jsx
+++ src/components/list.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
-export const List = ({ children, ...rest }) => {
+export const List = React.memo(({ children, ...rest }) => {
   return <ul {...rest}>{children}</ul>
-}
+})
 
-export const ListItem = ({ children, ...rest }) => {
+export const ListItem = React.memo(({ children, ...rest }) => {
   return <li {...rest}>{children}</li>
-}
+})
